c2231090e80e1a58c7c32d80c6e555340f47bab3
ione <ivan.polyakoff2003@gmail.com>
2023-04-24 23:56:03 +0300

    error 1

--- settings/base.py
+++ settings/base.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'apps.userlogin',
+    'apps.userlogin'
+    
 ]
 
 MIDDLEWARE = [
@@ -86,7 +86,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
-AUTH_USER_MODEL = 'userlogin.User'
+#AUTH_USER_MODEL = 'userlogin.User'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
