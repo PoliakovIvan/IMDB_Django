a43341d7d310044f1d78628dab03884936155669
ione <ivan.polyakoff2003@gmail.com>
2023-04-25 00:02:23 +0300

    error 2 with createsuperuser

--- apps/userlogin/models.py
+++ apps/userlogin/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
-
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 class User(AbstractUser):
     USERNAME_FIELDS = 'email'
@@ -9,3 +9,11 @@ class User(AbstractUser):
 
     username = None
     email = models.EmailField(_('email adrdress'), null=False, unique=True)
+    groups = models.ManyToManyField(Group, verbose_name='groups', blank=True, related_name='userlogin_users')
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        related_name='userlogin_users',
+        help_text='Specific permissions for this user.',
+    )
